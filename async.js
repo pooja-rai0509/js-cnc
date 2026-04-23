a01320619370efee68f0e4f2ee933368be84210d
Pooja Rai <117572904+pooja-rai0509@users.noreply.github.com>
2026-04-23 19:11:11 +0000

    async flow

---
+++ async.js
@@ -0,0 +1,13 @@
+// Javascript(default) - Synchronous, Single threaded
+
+// Execution Context - execute one line of code at a time
+// each operation waits for the last one to complete before executing
+// Call Stack 
+// Memory Heap
+
+// Blocking code vs Non-Blocking code
+// Blocking - Block the flow of program (Read file sync)
+// Non-Blocking - Does not block execution (Read file async)
+
+// 1. In stack one by one program executes 
+// 2. If any Web Api function called like setTimeout, setInterval then 1st other programs executes then that setTimeout function moves to queue then gos to stack & executes
